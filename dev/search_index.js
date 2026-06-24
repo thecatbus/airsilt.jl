f7ca848aa98abff5feba4ecf05c620cb5ce5a5c0
Documenter.jl <documenter@juliadocs.github.io>
2026-06-24 14:00:48 +0000

    build based on 4660062

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#AIRSilt.jl","page":"AIRSilt.jl","title":"AIRSilt.jl","text":"Tools for Adachi–Iyama–Reiten's tau-tilting theory.","category":"section"}]
+}
